f029d68f50591ac6a4843f608cb1399be7309bf7
Isha Dadhania <isha.dadhania@brainvire.com>
2025-04-10 18:33:01 +0530

    demo of  Template Driven formand its validation in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,33 +20,10 @@ import { User } from './interfaces/User';
   })
   export class AppComponent {
 
-   profileForm = new FormGroup({
-    name: new FormControl('', [Validators.required]),
-    email:new FormControl('', [Validators.required, Validators.maxLength(50), Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]),
-    password: new FormControl('', [Validators.required, Validators.minLength(5)])
-  });
-
-  onSubmit(){
-    console.log(this.profileForm.value);
-  }
-
-  get name() {
-    return this.profileForm.get('name');
-  }
-  
-  get email() {
-    return this.profileForm.get('email');
-  }
-  
-  get password() {
-    return this.profileForm.get('password');
-  }
-  setValue(){
-    this.profileForm.setValue({
-      name: 'purva',
-      email: 'purva@mailinator.com',
-      password: 'Brain@2029'
-    });
+  userDetail:any;
+  addDetails(val:NgForm){
+   console.log(val)
+   this.userDetail = val;
   }
 
 
