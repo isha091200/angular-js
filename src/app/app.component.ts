36a7941e3116b40eefeda0a4c0647e746b7136a7
Isha Dadhania <isha.dadhania@brainvire.com>
2025-04-10 18:29:24 +0530

    demo of  Reactive form validation  in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,16 +20,33 @@ import { User } from './interfaces/User';
   })
   export class AppComponent {
 
-  name = new FormControl('abc');
-  password = new FormControl('123');
+   profileForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    email:new FormControl('', [Validators.required, Validators.maxLength(50), Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]),
+    password: new FormControl('', [Validators.required, Validators.minLength(5)])
+  });
 
-  displayValue(){
-    console.log(this.name.value,this.password.value)
+  onSubmit(){
+    console.log(this.profileForm.value);
   }
 
+  get name() {
+    return this.profileForm.get('name');
+  }
+  
+  get email() {
+    return this.profileForm.get('email');
+  }
+  
+  get password() {
+    return this.profileForm.get('password');
+  }
   setValue(){
-    this.name.setValue('Isha');
-    this.password.setValue('Brain@2030');
+    this.profileForm.setValue({
+      name: 'purva',
+      email: 'purva@mailinator.com',
+      password: 'Brain@2029'
+    });
   }
 
 
