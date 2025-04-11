9008f843c991182608b01dd3f4b77515da488ad8
Isha Dadhania <isha.dadhania@brainvire.com>
2025-04-11 11:09:05 +0530

    demo of lazy loading in Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProfileComponent } from './profile/profile.component';
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    // {
+    //     path: 'admin',
+    //     component: AdminComponent
+    // }
+    {
+        path: 'admin',
+        loadComponent:()=>import('./admin/admin.component').then((c)=>c.AdminComponent)
+    }
+];
